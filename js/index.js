c09793c780580dba004be4b22298f4726de5e8a8
Ansh Gujral <anshgujral1997@gmail.com>
2018-11-10 23:19:27 +0530

    added typing animation

---
+++ js/index.js
@@ -0,0 +1,10 @@
+  	$("#typed").typed({
+  		strings: ["'M ANSH GUJRAL", " LOVE REACT", "'M A CODE ADDICT", "'M A FULL-STACK DEVELOPER"],
+  		typeSpeed: 100,
+  		startDelay: 0,
+  		backSpeed: 60,
+  		backDelay: 2000,
+  		loop: true,
+  		cursorChar: "|",
+  		contentType: 'html'
+  	});
